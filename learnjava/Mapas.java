fac15b045894f70aef543887ad0f7a971b8428a5
ajomacias <ajo.macias@yavirac.edu.ec>
2022-02-20 17:23:01 -0500

    cambios y aprendizaje

---
+++ learnjava/Mapas.java
@@ -0,0 +1,15 @@
+public class Mapas {
+    public String nombre;
+    public String apellido;
+    public static String contador;
+
+    public static void main(String[] args) {
+
+        
+    }
+
+    public static Crearmapa(){
+
+    }
+    
+}
